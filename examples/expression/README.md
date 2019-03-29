705cc9b1b982e2627c625587e904d69a17587f9d
= <merijnkruithof@hotmail.com>
2019-03-29 18:32:06 +0100

    added example

---
+++ examples/expression/README.md
@@ -0,0 +1,9 @@
+# Expression
+#### Warning: This example is under construction
+This example demonstrates binary operations, left-association, unary operations, and the order of operations.
+
+The main.go file executes the following expressions O(n):
+- 2
+- 2 + 2 // 4
+- 2 + 2 * 4 // 10
+- (2 + 2) * 4 // 16
---
+++ examples/expression/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/njirem95/simple-pascal/pkg/parser"
+	"github.com/njirem95/simple-pascal/pkg/scanner"
+	"github.com/njirem95/simple-pascal/pkg/visitor"
+	"log"
+)
+
+func main() {
+	inputs := [4]string{"2", "2 + 2", "2 + 2 * 4", "(2 + 2) * 4"}
+	for _, input := range inputs {
+		lexer, err := scanner.New(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		parser := parser.New(lexer)
+
+		expression, err := parser.Expr()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		visitor := visitor.New()
+		result := visitor.Visit(expression)
+
+		fmt.Println(result) // 4 (duh)
+	}
+
+}
